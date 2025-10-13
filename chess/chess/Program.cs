388cfc855f0ef7b7858ae3453d4c9692f4393e27
orlando <bowcotto297@kesbath.com>
2025-10-13 12:40:37 +0100

    done for class time

---
+++ chess/chess/Program.cs
@@ -0,0 +1,86 @@
+﻿
+
+/*
+ Documentation:
+ 1 = king
+ 2 = queen
+ 3 = bishop
+ 4 = knight
+ 5 = rook
+ 6 = pawn
+*/
+
+namespace chess
+{
+    class Program
+    {
+        static void DisplayBoard(Board chessBoard)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        Console.Write("   ");
+                    }
+                    else if (i == 0)
+                    {
+                        if (j == 8)
+                        {
+                            Console.Write(chessBoard.GetPositionUnit(i, j) + "\n");
+                        }
+                        else
+                        {
+                            Console.Write(chessBoard.GetPositionUnit(i, j) + " ");
+                        }
+
+                    }
+                    else if (j == 0)
+                    {
+                        Console.Write(chessBoard.GetPositionUnit(i, j) + "  ");
+                    }
+                    else if (chessBoard.GetPositionUnit(i, j) == 1)
+                    {
+                        Console.Write("K ");
+                    }
+                    else if (chessBoard.GetPositionUnit(i, j) == 2)
+                    {
+                        Console.Write("Q ");
+                    }
+                    else if (chessBoard.GetPositionUnit(i, j) == 3)
+                    {
+                        Console.Write("B ");
+                    }
+                    else if (chessBoard.GetPositionUnit(i, j) == 4)
+                    {
+                        Console.Write("N ");
+                    }
+                    else if (chessBoard.GetPositionUnit(i, j) == 5)
+                    {
+                        Console.Write("R ");
+                    }
+                    else if (chessBoard.GetPositionUnit(i, j) == 6)
+                    {
+                        Console.Write("P ");
+                    }
+                    else
+                    {
+                        Console.Write(". ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Board chessBoard = new Board();
+            
+            DisplayBoard(chessBoard);
+
+
+
+        }
+    }
+}
---
+++ chess/chess/board.cs
@@ -0,0 +1,125 @@
+﻿public class Board
+{
+    private int[,] BoardArr;
+    private int[,] PlayersArr;
+
+    public Board()
+    {
+        BoardArr = new int[,]
+        {
+            { 0, 0, 1, 2, 3, 4, 5, 6, 7 },
+            { 0, 5, 4, 3, 1, 2, 3, 4, 5 },
+            { 1, 6, 6, 6, 6, 6, 6, 6, 6 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 3, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 4, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 5, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 6, 6, 6, 6, 6, 6, 6, 6, 6 },
+            { 7, 5, 4, 3, 2, 1, 3, 4, 5 }
+        };
+        PlayersArr = new int[,]
+        {
+            {1,1,1,1,1,1,1,1 },
+            {1,1,1,1,1,1,1,1 },
+            {0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0 },
+            {2,2,2,2,2,2,2,2 },
+            {2,2,2,2,2,2,2,2 }
+        }
+    }
+    public int GetPositionUnit(int X, int Y)
+    {
+        int ThisPosition = BoardArr[X, Y];
+        return ThisPosition;
+    }
+    public void SetPosition(int X, int Y, int PieceID)
+    {
+        BoardArr[X, Y] = PieceID;
+    }
+    public void MovePiece(int ChosenX, int ChosenY)
+    {
+        int SelectedPiece = GetPositionUnit(ChosenX, ChosenY);
+        switch (SelectedPiece)
+        {
+            case 1: // King
+                    // Implement king movement logic
+                break;
+            case 2: // Queen
+                    // Implement queen movement logic
+                break;
+            case 3: // Bishop
+                    // Implement bishop movement logic
+                break;
+            case 4: // Knight
+                    // Implement knight movement logic
+                break;
+            case 5: // Rook
+                    // Implement rook movement logic
+                break;
+            case 6: // Pawn
+                    // Implement pawn movement logic
+                break;
+            default:
+                // No piece selected or invalid piece
+                break;
+        }
+    }
+    public string[] GetMovementOptions(int ChosenX, int ChosenY)
+    {
+        if (ChosenX < 1 || ChosenX > 8 || ChosenY < 1 || ChosenY > 8)
+        {
+            return new string[] { "Invalid coordinates" };
+        }
+
+        int SelectedPiece = GetPositionUnit(ChosenX, ChosenY);
+
+        switch (SelectedPiece)
+        {
+            case 1:
+                bool[] ValidMoves = new bool[8];
+                if ()
+
+                break;
+            case 2: // Queen
+                    // Implement queen movement logic
+                break;
+            case 3: // Bishop
+                    // Implement bishop movement logic
+                break;
+            case 4: // Knight
+                    // Implement knight movement logic
+                break;
+            case 5: // Rook
+                    // Implement rook movement logic
+                break;
+            case 6: // Pawn
+                    // Implement pawn movement logic
+                break;
+            default:
+                // No piece selected or invalid piece
+                break;
+        }
+    }
+
+    /*
+     Documentation:
+    pieces
+     1 = king
+     2 = queen
+     3 = bishop
+     4 = knight
+     5 = rook
+     6 = pawn
+
+    movement options:
+    king = 1 up 2 up right 3 right 4 down right 5 down 6 down left 7 left 8 up left no distance modifier
+    queen = 1 up 2 up right 3 right 4 down right 5 down 6 down left 7 left 8 up left distance modifier 
+    bishop = 2 up right 4 down right 6 down left 8 up left distance modifier
+    knight = 1 up up right 2 right right up 3 right right down 4 down down right 5 down down left 6 left left down 7 left left up 8 up up left no distance modifier
+    rook = 1 up 3 right 5 down 7 left distance modifier
+    pawn = 1 up 2 up up first move only 3 up right attack 4 up left attack no distance modifier
+    */
+
+}
