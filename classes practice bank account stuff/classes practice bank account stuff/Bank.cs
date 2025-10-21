8f22ee239b7f77cf70496f2ca5f8ea520e4d8566
orlando <bowcotto297@kesbath.com>
2025-10-21 12:42:53 +0100

    done for tuesday

---
+++ classes practice bank account stuff/classes practice bank account stuff/Bank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Bank 
+{
+	private int SortCode;
+	private string BankName;
+	private BankAccount[] accounts = new BankAccount[100]; 
+
+	public Bank(int SortCode, string BankName)
+	{
+		this.SortCode = SortCode;
+		this.BankName = BankName;
+	}
+
+
+	
+}
