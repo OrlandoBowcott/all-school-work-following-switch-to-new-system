ba6c89de8affd739e96daee0fa7699548b7fc7c3
orlando <bowcotto297@kesbath.com>
2025-10-30 16:27:24 +0000

    done finding answer number

--- Q4 big boi/Q4 big boi/Program.cs
+++ Q4 big boi/Q4 big boi/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Q4_big_boi
+﻿using System;
+
+namespace Q4_big_boi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string answer = "";
+            for (int a = 0; a < 6; a++)
+            {
+                for (int b = 0; b < 6; b++)
+                {
+                    for (int c = 0; c < 6; c++)
+                    {
+                        for (int d = 0; d < 6; d++)
+                        {
+                            for (int e = 0; e < 6; e++)
+                            {
+                                if ( a == b || a == c || a == d || a == e ||
+                                    b == c || b == d || b == e||
+                                    c == d || c == e ||
+                                    d == e)
+                                {
+                                    continue;
+                                }
+                                if (2025 == 5 * 15 * a + 5 * 15 * b + 5 * 27 * c + 5 * 27 + d + 15 * 27 * e)
+                                {
+                                    answer = Convert.ToString(a) + Convert.ToString(b) + Convert.ToString(c) + Convert.ToString(d) + Convert.ToString(e);
+                                    Console.WriteLine($"5 * 15 * {a} + 5 * 15 * {b} + 5 * 27 * {c} + 5 * 27 + {d} + 15 * 27 * {e} = {5 * 15 * a + 5 * 15 * b + 5 * 27 * c + 5 * 27 + d + 15 * 27 * e}");
+                                    Console.WriteLine(answer);
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
