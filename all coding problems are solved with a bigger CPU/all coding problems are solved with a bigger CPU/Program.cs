f41d4f14d4a9a8fd1530cab5676a84b0acc379c3
orlando <bowcotto297@kesbath.com>
2025-10-30 14:46:26 +0000

    intial commit of problem 3 code

---
+++ all coding problems are solved with a bigger CPU/all coding problems are solved with a bigger CPU/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace all_coding_problems_are_solved_with_a_bigger_CPU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 111111; i <= 666666; i++)
+            {
+               
+            }
+        }
+    }
+}
