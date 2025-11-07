00f0a736246a16ba983c7b398ab05578ec9c1c56
orlando <bowcotto297@kesbath.com>
2025-11-07 14:45:06 +0000

    new classes mad and added. using abstract classes

---
+++ polymorphism practics/polymorphism practics/animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Animal
+{
+	private string name;
+	private int age;
+	public Animal(string name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+
+	public abstract speak()
+	{
+	}
+}
---
+++ polymorphism practics/polymorphism practics/cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Cat : Animal
+{
+	private string breed;
+	private int evilnessFactor;
+
+	public Cat(string breed, int evilnessfactor) : Animal(string name, int age)
+	{
+		this.breed = breed;
+		this.evilnessFactor = evilnessFactor;
+    }
+	{
+
+	}
+}
